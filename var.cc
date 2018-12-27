4a9f5902d8f3530f3034eed209b8e32d164b0808
egrlove <egrlove@yandex.ru>
2018-12-27 19:22:49 +0400

    Jule_help

--- var.cc
+++ var.cc
@@ -100,6 +100,12 @@ int main()
   double x[n];
   double xk[n], p[n];
   double dPhi[n];
+  double Phi[n];
+  for (int i = 0; i < n; i++)
+  {
+    Phi[i] = 0.0;
+    // xk[i] = 15;
+  }
 
   // �������� ��������� �����������
   double I_n1 = 0.0;
@@ -109,28 +115,26 @@ int main()
   double U_n1_C3 = 0.0;
   double U_n1_C4 = 0.0;
   double U_n1_C5 = 0.0;
+
+  double fi4 = 0.0;
+
   xk[0] = 0.0;
   xk[1] = 0.0;
   xk[2] = 0.0;
   xk[3] = 0.0;
+  printf("=======================================================================\n");
+  printf("=======================================================================\n");
+  printf("=======================================================================\n");
+
   while (now_T < T)
   {
 
     do
     {
-      if (iter)
-      {
-        for (int i = 0; i < n; i++)
-        {
-          xk[i] = a[i][n];
-          // xk[i] = 15;
-        }
-      }
       iter++;
       {
         double P = 1e-4;
-        // E = E_n;
-        I_n1 = I_n1 + (delta_T / L) * (-xk[0]);
+        I_n1 = I_n1 + (delta_T / L) * (-Phi[0]);
         // U_n1_C3 = xk[3];
         // U_n1_C4 = xk[1];
         // U_n1_C5 = xk[2] - xk[3];
@@ -138,27 +142,10 @@ int main()
         E = E_n * sin((2 * 3.14 * now_T) / P);
         I2 = It * (exp(U_n1_C5 / mFt) - 1.0);
 
-        a[0][4] = -(
-            -(I_n1 + (delta_T / L) * xk[0]) + E / R1);
-        a[1][4] = -(-E / R1  +
-                    (C4 / delta_T * (xk[1] - U_n1_C4)) + (xk[1] - xk[2]) / R2);
-        a[2][4] = -(-(xk[1] - xk[2]) / R2 +
-                    (C5 / delta_T * (xk[2] - xk[3] - U_n1_C5)) +
-                    (xk[2] - xk[3]) / R3 + I2);
-        a[3][4] = -(-(C5 / delta_T * (xk[2] - xk[3] - U_n1_C5)) -
-                    (xk[2] - xk[3]) / R3 - I2 +
-                    (C3 / delta_T * (xk[3] - U_n1_C3)));
-
-        // a[0][4] = -(
-        //     -(I_n1 + (delta_T / L) * xk[0]) + E / R1 + (xk[0] - xk[1]) / R1);
-        // a[1][4] = -(-E / R1 - (xk[0] - xk[1]) / R1 +
-        //             (C4 / delta_T * (xk[1] - U_n1_C4)) + (xk[1] - xk[2]) / R2);
-        // a[2][4] = -(-(xk[1] - xk[2]) / R2 +
-        //             (C5 / delta_T * (xk[2] - xk[3] - U_n1_C5)) +
-        //             (xk[2] - xk[3]) / R3 + I2);
-        // a[3][4] = -(-(C5 / delta_T * (xk[2] - xk[3] - U_n1_C5)) -
-        //             (xk[2] - xk[3]) / R3 - I2 +
-        //             (C3 / delta_T * (xk[3] - U_n1_C3)));
+        a[0][4] = -(-(I_n1 + (delta_T / L) * Phi[0]) + E / R1);
+        a[1][4] = -(-E / R1 + (C4 / delta_T * (Phi[1] - U_n1_C4)) + (Phi[1] - Phi[2]) / R2);
+        a[2][4] = -(-(Phi[1] - Phi[2]) / R2 + (C5 / delta_T * (Phi[2] - Phi[3] - U_n1_C5)) + (Phi[2] - Phi[3]) / R3 + I2);
+        a[3][4] = -(-(C5 / delta_T * (Phi[2] - Phi[3] - U_n1_C5)) - (Phi[2] - Phi[3]) / R3 - I2 + (C3 / delta_T * (Phi[3] - U_n1_C3)));
       }
 
       {
@@ -184,38 +171,15 @@ int main()
       }
       // ��������� ������ ������ ������
 
-      {
-          // a[0][0] = (delta_T / L);
-          // a[0][1] = 0.0;
-          // a[0][2] = 0.0;
-          // a[0][3] = 0.0;
-
-          // a[1][0] = 0.0;
-          // a[1][1] =  (C4 / delta_T) + (1 / R2);
-          // a[1][2] = -(1 / R2);
-          // a[1][3] = 0.0;
-
-          // a[2][0] = 0.0;
-          // a[2][1] = -(1 / R2);
-          // a[2][2] = (1 / R2) + (C5 / delta_T) + (1 / R3);
-          // a[2][3] = -(C5 / delta_T) - (1 / R3);
-
-          // a[3][0] = 0.0;
-          // a[3][1] = 0.0;
-          // a[3][2] = -(C5 / delta_T) - (1 / R3);
-          // a[3][3] = (C5 / delta_T) + (1 / R3) + (C3 / delta_T);
-      }
-
-
-      // print_matr(a);
+      print_matr(a);
       gauss_forward(a, n, n + 1);
       gauss_reverse(a, n, n + 1);
       // print_matr(a);
 
       for (int i = 0; i < n; i++)
       {
-        dPhi[i] = (a[i][n] - xk[i]);
-        // dPhi[i] = fabs(a[i][n]);
+        // dPhi[i] = (a[i][n] - xk[i]);
+        dPhi[i] = (a[i][n]);
       }
 
       if (sup_norm(dPhi) < eps1)
@@ -232,19 +196,34 @@ int main()
         can_step = false;
         delta_T = delta_T / 2.0;
       }
-      // } while (norm(xk, a) <= eps);
-    } while (not can_step);
-    // a[i][n]
-    // U_n1_C3 = xk[3];
-    // U_n1_C4 = xk[1];
-    // U_n1_C5 = xk[2] - xk[3];
 
-    U_n1_C3 = a[3][n];
-    U_n1_C4 = a[1][n];
-    U_n1_C5 = a[2][n] - a[3][n];
+      for (int i = 0; i < n; i++)
+      {
+        Phi[i] += dPhi[i];
+        // xk[i] = 15;
+      }
+
+      if(iter>25){
+        // cout<<"ne soshlos' =("<<endl;
+         return(0);
+       }
 
 
-    fprintf(file, "%.20lf    %.10lf     \n", now_T, (a[3][n]));
+      } while (sup_norm(dPhi) >= eps);
+    // } while (not can_step);
+  iter = 0;
+    // fi4 += a[3][n];
+    // U_n1_C3 = a[3][n];
+    // U_n1_C4 = a[1][n];
+    // U_n1_C5 = a[2][n] - a[3][n];
+
+    U_n1_C3 = Phi[3];
+    U_n1_C4 = Phi[1];
+    U_n1_C5 = Phi[2] - Phi[3];
+
+    // fprintf(file, "%.20lf    %.10lf     \n", now_T, ([3][n]));
+    fprintf(file, "%.20lf    %.10lf     \n", now_T, Phi[n]);
+
     fflush(file);
 
     // printf("%s", "������ �������: [ ");
